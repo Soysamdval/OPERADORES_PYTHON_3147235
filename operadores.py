38b27c96d970522038da5fb0aca8b3076e9ccddd
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-03-07 06:36:56 -0500

    subido con éxito

--- operadores.py
+++ operadores.py
@@ -14,5 +14,22 @@ a = 3
 b = 2
 c = 1
 x= 5
-y= ((2*a +c)/7)* (a+(4*a)/c)
+y=x + 2 * a - c* 3 != b+x*a-4+b
 print (y);
+
+'''
+OPERADORES RELACIONALES:
+Las operaciones relacionales resuktan en un valor booleano:
+true false (v, f, si, no)
+
+OPERADORES RELACIONALES:
+>, <, >=, <=, !=, ==
+JERARQUIA DE OPERADORES INCLUYENDO RELACIONALES
+
+1. ()
+2. **
+3. *, /, %
+4. +, -
+5. >, <, >=, <=, !=, ==
+6. =
+'''
