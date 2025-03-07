08a1b10acdee03816780286c08d57eb31ae68689
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-03-07 08:57:24 -0500

    subido con éxito

---
+++ Operadores_logicos.py
@@ -0,0 +1,30 @@
+'''
+OPERADORES LÓGICOS
+LOS OPERADORES LÓGICOS SON:
+AND, OR, NOT.
+
+OBEDENCEN LAS TABLAS DE VERDAD:
+'''
+
+op1 = False
+op2 = True
+op3 = op1 or op2
+print (op3)
+
+#operador not
+
+op4 = not op1
+print (op4)
+
+'''
+JERARQUÍ DEFINITIVA DE OPERACIONES
+1. ()
+2. **
+3. *, /, %
+4. +,-
+5. >, <, !=, ==, <=, >=
+6. NOT
+7. AND
+8. OR
+9. =
+'''
