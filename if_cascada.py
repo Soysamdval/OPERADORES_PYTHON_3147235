9cbc0e7592b3cb5a75f6bf255b3fe725c62b47da
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-03-19 10:32:04 -0500

    subido

---
+++ if_cascada.py
@@ -0,0 +1,5 @@
+fruta_es = input('escribe tu fruta')
+if fruta_es == "manzana" or "Manzana" or "MANZANA":
+    print("Apple")
+elif fruta_es == "naranja" or "Naranja" or "NARANJA":
+    print("Orange")
