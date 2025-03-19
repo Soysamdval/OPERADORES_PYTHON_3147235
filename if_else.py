11f1226196822da36ca3fdb3dbfbf3f34d52f1de
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-03-19 07:47:50 -0500

    if_else

---
+++ if_else.py
@@ -0,0 +1,23 @@
+'''
+if else:
+
+sintaxis:
+    if condicional:
+        instruccion1
+        instruccion2
+    else:
+        instruccion3
+        instruccion4
+'''
+
+edad = 20
+documento = False
+
+if edad >=18 and documento == True :
+    print("Usted puede votar")
+    print("Usted si puede votar")
+else:
+    print("Usted no puede votar")
+    print("O")
+    print("Usted no puede votar")
+print("Fin del programa")
