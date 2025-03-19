f4884b7f3abd66ac7b4ca41d3aead4b1f543c991
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-03-19 08:25:27 -0500

    submit

---
+++ actividad1.py
@@ -0,0 +1,9 @@
+temperatura = 90
+if temperatura >= 80:
+    print("Su motor debe apagarse")
+    print ("motor apagado")
+
+else: 
+    print ("el motor está en la temperatura correcta")
+    print ("Motor continúa encendido")
+print ("proceso finalizado")
