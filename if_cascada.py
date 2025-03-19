c2de66961fda7b011fb938e244250da9cdccb3f5
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-03-19 10:40:00 -0500

    subida

--- if_cascada.py
+++ if_cascada.py
@@ -3,3 +3,9 @@ if fruta_es == "manzana" or "Manzana" or "MANZANA":
     print("Apple")
 elif fruta_es == "naranja" or "Naranja" or "NARANJA":
     print("Orange")
+elif fruta_es == "pera" or "Pera" or "PERA":
+    print("Pear")
+elif fruta_es == "uvas" or "Uvas" or "UVAS":
+    print("Graps")
+elif fruta_es == "piña" or "Piña" or "PIÑA":
+    print("Pineaple")
